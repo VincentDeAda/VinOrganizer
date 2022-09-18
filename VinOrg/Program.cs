cd67fc6233c9a0d064f6f28c656b4c1f30a4dc50
Captain Sh4dow <CaptainSh4dow@gmail.com>
2022-09-18 22:21:51 +0300

    Implemented setup-common-extensions Command.

--- VinOrg/Program.cs
+++ VinOrg/Program.cs
@@ -95,6 +95,36 @@ app.AddCommand("add", ([FromService] SQLiteDatabase db, [Argument] List<string> 
     db.SaveChanges();
 });
 
+app.AddCommand("setup-common-extensions", ([FromService] SQLiteDatabase db, [Option('y')] bool confirm) =>
+{
+    if (!confirm)
+    {
+        Console.WriteLine("This action will erase all current saved extension packs.");
+        Console.Write("Press [Y] To Proceed Further : ");
+        var k = Console.ReadKey();
+        if (k.Key != ConsoleKey.Y)
+            return;
+    }
+
+
+    Func<ICollection<string>, List<Extension>> stringToExt = (x) => x.Select(y => new Extension() { ExtensionName = y }).ToList();
+
+
+    var commonExt = new List<ExtensionPack>()
+    {
+        new (){ Name="Programs", Extensions= stringToExt(new List<string>{"exe", "msi", "jar", "csh", "bat", "reg", "vsix" }) },
+        new (){ Name="Video", Extensions= stringToExt(new List<string>{"mp4", "mkv", "flv", "webm", "vob", "ogv", "drc", "mng", "avi", "ts", "mov", "qt", "wmv", "m4p", "m4v"  }) },
+        new (){ Name="Images", Extensions= stringToExt(new List<string>{"png", "jpg", "gif", "jpeg", "svg", "webp", "bmp", "ico", "tif"  }) },
+        new (){ Name="Compressed", Extensions= stringToExt(new List<string>{"zip", "rar", "7z", "gz", "iso", "tar", "lz", "lz4"  }) },
+        new (){ Name="Audio", Extensions= stringToExt(new List<string>{"mp3", "wav", "flac", "acc", "wma", "weba"}) },
+        new (){ Name="Documents", Extensions= stringToExt(new List<string>{"doc", "docx", "pdf", "txt", "csv", "xlsx", "srt"}) },
+    };
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+    db.AddRange(commonExt);
+    db.SaveChanges();
+});
+
 app.AddSubCommand("remove", x =>
 {
 
