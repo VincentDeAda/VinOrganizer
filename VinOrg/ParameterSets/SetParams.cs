db1bfdc37db035ebddfc8f26a92113ec156616b2
Captain Sh4dow <CaptainSh4dow@gmail.com>
2022-09-22 02:35:25 +0300

    Documented The Set Command.

--- VinOrg/ParameterSets/SetParams.cs
+++ VinOrg/ParameterSets/SetParams.cs
@@ -3,12 +3,12 @@ namespace VinOrgCLI.ParameterSets;
 
 internal class SetParams : ICommandParameterSet
 {
-    [Argument]
+    [Argument(Description ="The name of the pack requested.")]
     public string PackName { get; set; } = null!;
-    [Option('p'), HasDefaultValue]
+    [Option('p', Description = "Specify new path for the requested pack."), HasDefaultValue]
     public string? NewPath { get; set; }
-    [Option('n'),HasDefaultValue]
+    [Option('n', Description = "Specify new name for the requested pack."),HasDefaultValue]
     public string? NewName { get; set; }
-    [Option('y')]
+    [Option('y', Description = "Automatically confirm the requested action.")]
     public bool Confirm { get; set; }
 }
