368846003defc3752165692d4833ad8c95570fd3
Captain Sh4dow <CaptainSh4dow@gmail.com>
2022-10-06 19:36:49 +0300

    Moved the set command to the logic class.

--- Shared.Logic/DataHandler .cs
+++ Shared.Logic/DataHandler .cs
@@ -41,10 +41,39 @@ public static class DataHandler
 			else
 				extPacks.Add(extPack);
 		}
-		if(invalidLists.Count > 0) return invalidLists;
+		if (invalidLists.Count > 0) return invalidLists;
 		extPacks.ForEach(x => x.Extensions.ForEach(target.Extensions.Add));
 		extPacks.ForEach(x => db.ExtensionPacks.Remove(x));
 		db.SaveChanges();
 		return null;
+	}
+
+	public static List<string>? SetPaths(this IExtensionsPacksRepository db, List<string> packs, string path)
+	{
+		var invalidPacks = new List<string>();
+		var extPacks = new List<ExtensionPack>();
+		foreach (string pack in packs)
+		{
+			var extPack = db.ExtensionPacks.FirstOrDefault(x => x.Name == pack);
+			if (extPack is null) invalidPacks.Add(pack);
+			else extPacks.Add(extPack);
+		}
+		if (invalidPacks.Count > 0) return invalidPacks;
+
+		extPacks.ForEach(x => x.Path = path);
+		db.SaveChanges();
+		return null;
+	}
+
+	public static byte SetName(this IExtensionsPacksRepository db, string name, string newName)
+	{
+		var pack = db.ExtensionPacks.FirstOrDefault(x => x.Name == name);
+		if (pack is null) return 1;
+		var targetPack = db.ExtensionPacks.FirstOrDefault(x => x.Name == newName);
+		if (targetPack is not null) return 2;
+
+		pack.Name = newName;
+		db.SaveChanges();
+		return 0;
 	}
 }
--- VinOrg/Commands/SetCommand.cs
+++ VinOrg/Commands/SetCommand.cs
@@ -4,42 +4,31 @@ internal class SetCommand
 	private readonly IExtensionsPacksRepository _db;
 
 	public SetCommand(IExtensionsPacksRepository db) => _db = db;
-	[Command(Aliases = new[] { "s" }, Description = "Update extension pack name or path.")]
-	public void Set(SetParams paramSet)
+	[Command(Aliases = new[] { "p" })]
+	public void Path([Argument] List<string> packs, [Argument, PathValid] string path)
 	{
 
-		if (paramSet.NewPath is null && paramSet.NewName is null)
+		var invalidPacks = _db.SetPaths(packs, path);
+
+		if (invalidPacks is not null)
 		{
-			Console.WriteLine("There's no action requested.");
-			return;
+			Console.WriteLine("The following packs doesn't exist on the database: ");
+			invalidPacks.ForEach(Console.WriteLine);
 		}
-		var pack = _db.ExtensionPacks.FirstOrDefault(x => x.Name == paramSet.PackName);
-		if (pack is null)
+	}
+	public void Name([Argument] string pack, [Argument,IsValidPackName] string newName)
+	{
+		var res= _db.SetName(pack, newName);
+		switch (res)
 		{
-			Console.WriteLine("The provided pack name doesn't exist.");
-			return;
+			case 1:
+				Console.WriteLine("The provided pack name doesn't exist on the database.");
+				break;
+			case 2:
+				Console.WriteLine("The new pack name already in use.");
+				break;
+			default:
+				break;
 		}
-		if (paramSet.NewName is not null)
-		{
-			if (_db.ExtensionPacks.FirstOrDefault(x => x.Name == paramSet.NewName) is not null)
-			{
-				Console.WriteLine("Pack name already used");
-				return;
-			}
-			pack.Name = paramSet.NewName;
-		}
-		if (paramSet.NewPath is not null)
-		{
-			var absPath = Path.GetFullPath(paramSet.NewPath);
-			if (!paramSet.Confirm)
-			{
-				Console.Write("Press [Y] to confirm updating the path for {0} to {1}. : ", pack.Name, absPath);
-				var k = Console.ReadKey();
-				if (k.Key != ConsoleKey.Y)
-					return;
-			}
-			pack.Path = absPath;
-		}
-		_db.SaveChanges();
 	}
 }
