4f39c37b30813cbd3d2a70c8a8f51ea08f436182
Captain Sh4dow <CaptainSh4dow@gmail.com>
2022-10-09 23:25:40 +0300

    Implemented ProgressTracker Interface.

---
+++ Shared.Logic/IProgressTracker.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Logic;
+public interface IProgressTracker
+{
+	public int Current { get; set; }
+	public int Max { get;  set; }
+	void Increament(int? value);
+	void SetMax(int max);
+}
