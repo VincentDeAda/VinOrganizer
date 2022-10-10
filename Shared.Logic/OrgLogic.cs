fb1a180562efa08c9e994d88998b888ec58542d5
VincentDeAda <CaptainSh4dow@gmail.com>
2022-10-10 23:48:21 +0300

    Implemented IProgressTracker Inside OrgLogic Class.

--- Shared.Logic/OrgLogic.cs
+++ Shared.Logic/OrgLogic.cs
@@ -6,123 +6,124 @@ using System.Runtime.InteropServices;
 namespace Shared.Logic;
 public static class OrgLogic
 {
-	public static OrganizeResult? Organize(this IExtensionsPacksRepository db, string? path, int? recursionDepth, bool steal, bool recursive, bool moveUncategorized, bool noLog, bool autoRename)
-	{
-		var result = new OrganizeResult();
-		var currentDir = Directory.GetCurrentDirectory();
+  public static OrganizeResult? Organize(this IExtensionsPacksRepository db, string? path, int? recursionDepth, bool steal, bool recursive, bool moveUncategorized, bool noLog, bool autoRename, IProgressTracker? progressTracker = null)
+  {
+    var result = new OrganizeResult();
+    var currentDir = Directory.GetCurrentDirectory();
 
-		string lookupFolder = path ?? currentDir;
-		string targetFolder = steal ? currentDir : lookupFolder;
+    string lookupFolder = path ?? currentDir;
+    string targetFolder = steal ? currentDir : lookupFolder;
 
-		//Checking if dir is not dangerous dir.
-		if (IsBadDir(currentDir, lookupFolder))
-			return null;
-
-
-		var files = GetFiles(lookupFolder, recursive, recursionDepth);
-
-		//
-		if (moveUncategorized)
-			files = files.Where(x => db.Extensions.FirstOrDefault(y => x.Extension.Length > 0 ? x.Extension.Substring(1).ToLower() == y : false) != null).ToList();
-
-		//Grouping the files by their extension
-		var groupedFiles = files.GroupBy(x => x.Extension.ToLower());
+    //Checking if dir is not dangerous dir.
+    if (IsBadDir(currentDir, lookupFolder))
+      return null;
 
 
+    var files = GetFiles(lookupFolder, recursive, recursionDepth);
+        progressTracker?.SetMax(files.Count);
+    //
+    if (moveUncategorized)
+      files = files.Where(x => db.Extensions.FirstOrDefault(y => x.Extension.Length > 0 ? x.Extension.Substring(1).ToLower() == y : false) != null).ToList();
 
-		//Creating The Logger
-		LogManager? changeLogger;
+    //Grouping the files by their extension
+    var groupedFiles = files.GroupBy(x => x.Extension.ToLower());
 
-		if (noLog)
-			changeLogger = null;
-		else
-			changeLogger = LogManager.CreateLogger();
 
-		foreach (var ext in groupedFiles)
-		{
 
-			ExtensionPack? pack = null;
+    //Creating The Logger
+    LogManager? changeLogger;
 
-			//Checking if the extension valid and finding it in the db.
-			if (ext.Key.Length > 0)
-				pack = db.ExtensionPacks.FirstOrDefault(x => x.Extensions.Contains(ext.Key[1..].ToLower()));
+    if (noLog)
+      changeLogger = null;
+    else
+      changeLogger = LogManager.CreateLogger();
 
-			//If the extension doesn't exist on the db ignore it.
-			if (pack is null && moveUncategorized == false)
-				continue;
+    foreach (var ext in groupedFiles)
+    {
 
-			//Assigning the path to either the pack custom path or the target folder with checking if 
-			var extPackDir = Path.Combine(pack?.Path ?? targetFolder, pack?.Name ?? "Uncategorized");
+      ExtensionPack? pack = null;
 
-			try
-			{
-				Directory.CreateDirectory(extPackDir);
-			}
-			catch (Exception)
-			{
-				result.FailedToCreateDir++;
-				continue;
-			}
+      //Checking if the extension valid and finding it in the db.
+      if (ext.Key.Length > 0)
+        pack = db.ExtensionPacks.FirstOrDefault(x => x.Extensions.Contains(ext.Key[1..].ToLower()));
 
-			foreach (FileInfo file in CollectionsMarshal.AsSpan(ext.ToList()))
-			{
-				var log = new LogFile();
-				log.From = file.FullName;
-				try
-				{
-					file.MoveTo(Path.Combine(extPackDir, file.Name));
-					log.To = file.FullName;
-					result.FileMoved++;
-				}
-				catch (Exception)
-				{
-					if (autoRename)
-					{
-						file.MoveTo(Path.Combine(extPackDir, string.Format("{1}-{0}", file.CreationTime.ToString("yyyyMMddHHmmssff") + Random.Shared.Next(1000).ToString(), file.Name)));
-						log.To = file.FullName;
-						result.Renamed++;
-						result.FileMoved++;
+      //If the extension doesn't exist on the db ignore it.
+      if (pack is null && moveUncategorized == false)
+        continue;
 
-					}
-					else
-					{
-						result.FailedToMove++;
-					}
-				}
+      //Assigning the path to either the pack custom path or the target folder with checking if 
+      var extPackDir = Path.Combine(pack?.Path ?? targetFolder, pack?.Name ?? "Uncategorized");
 
-				changeLogger?.Log(log);
-			}
-		}
-		changeLogger?.DumpLog();
-		return result;
-	}
+      try
+      {
+        Directory.CreateDirectory(extPackDir);
+      }
+      catch (Exception)
+      {
+        result.FailedToCreateDir++;
+        continue;
+      }
 
-	private static List<FileInfo> GetFiles(string lookupFolder, bool recursive, int? maxRecursionDepth)
-	{
-		return Directory.GetFiles(lookupFolder
-				 , "*", new EnumerationOptions()
-				 {
-					 MaxRecursionDepth = maxRecursionDepth ?? int.MaxValue,
-					 IgnoreInaccessible = true,
-					 RecurseSubdirectories = recursive
-				 }).Select(x => new FileInfo(x)).ToList();
-	}
-	private static bool IsBadDir(string currentDir, string lookupFolder)
-	{
-		List<Environment.SpecialFolder> systemDirs = new List<Environment.SpecialFolder>() {
-		Environment.SpecialFolder.ApplicationData,
-		Environment.SpecialFolder.LocalApplicationData,
-		Environment.SpecialFolder.CommonApplicationData,
-		Environment.SpecialFolder.Windows,
-		Environment.SpecialFolder.System,
-		Environment.SpecialFolder.SystemX86,
-		Environment.SpecialFolder.ProgramFiles,
-		Environment.SpecialFolder.ProgramFilesX86,
+      foreach (FileInfo file in CollectionsMarshal.AsSpan(ext.ToList()))
+      {
+        var log = new LogFile();
+        log.From = file.FullName;
+        try
+        {
+          file.MoveTo(Path.Combine(extPackDir, file.Name));
+          log.To = file.FullName;
+          result.FileMoved++;
+        }
+        catch (Exception)
+        {
+          if (autoRename)
+          {
+            file.MoveTo(Path.Combine(extPackDir, string.Format("{1}-{0}", file.CreationTime.ToString("yyyyMMddHHmmssff") + Random.Shared.Next(1000).ToString(), file.Name)));
+            log.To = file.FullName;
+            result.Renamed++;
+            result.FileMoved++;
 
-	};
-		bool isSystemDir = systemDirs.Any(x => currentDir == Environment.GetFolderPath(x));
-		string sysDir = string.IsNullOrWhiteSpace(Environment.SystemDirectory) ? "/" : Environment.SystemDirectory;
-		bool isSystemRoot = Directory.GetDirectoryRoot(sysDir) == lookupFolder;
-		return isSystemDir || isSystemRoot;
-	}
+          }
+          else
+          {
+            result.FailedToMove++;
+          }
+        }
+
+        changeLogger?.Log(log);
+                progressTracker?.Increament(1);
+      }
+    }
+    changeLogger?.DumpLog();
+    return result;
+  }
+
+  private static List<FileInfo> GetFiles(string lookupFolder, bool recursive, int? maxRecursionDepth)
+  {
+    return Directory.GetFiles(lookupFolder
+         , "*", new EnumerationOptions()
+         {
+           MaxRecursionDepth = maxRecursionDepth ?? int.MaxValue,
+           IgnoreInaccessible = true,
+           RecurseSubdirectories = recursive
+         }).Select(x => new FileInfo(x)).ToList();
+  }
+  private static bool IsBadDir(string currentDir, string lookupFolder)
+  {
+    List<Environment.SpecialFolder> systemDirs = new List<Environment.SpecialFolder>() {
+    Environment.SpecialFolder.ApplicationData,
+    Environment.SpecialFolder.LocalApplicationData,
+    Environment.SpecialFolder.CommonApplicationData,
+    Environment.SpecialFolder.Windows,
+    Environment.SpecialFolder.System,
+    Environment.SpecialFolder.SystemX86,
+    Environment.SpecialFolder.ProgramFiles,
+    Environment.SpecialFolder.ProgramFilesX86,
+
+  };
+    bool isSystemDir = systemDirs.Any(x => currentDir == Environment.GetFolderPath(x));
+    string sysDir = string.IsNullOrWhiteSpace(Environment.SystemDirectory) ? "/" : Environment.SystemDirectory;
+    bool isSystemRoot = Directory.GetDirectoryRoot(sysDir) == lookupFolder;
+    return isSystemDir || isSystemRoot;
+  }
 }
