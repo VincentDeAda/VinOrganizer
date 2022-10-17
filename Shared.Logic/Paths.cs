897f929a22752cce81e4220dc5106306a0cb1d97
Captain Sh4dow <CaptainSh4dow@gmail.com>
2022-10-17 22:42:22 +0300

    Minor fix
    Fixed recalculating paths each time it get accessed.

--- Shared.Logic/Paths.cs
+++ Shared.Logic/Paths.cs
@@ -1,12 +1,6 @@
 ﻿namespace Shared.Logic;
 public static class Paths
 {
-    public static string ConfigDir
-    {
-        get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VDA", "VinOrg");
-    }
-    public static string LogDir
-    {
-        get => Path.Combine(ConfigDir, "Logs");
-    }
+	public static string ConfigDir { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "VDA", "VinOrg");
+	public static string LogDir { get; } = Path.Combine(ConfigDir, "Logs");
 }
